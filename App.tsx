52835451337d33ce6f0935190e4c898179f063b2
Nikholau <nikholasyan12@gmail.com>
2024-01-06 21:04:18 -0300

    feat: add components to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleRemoveItem = (item) => {
+    const updatedItems = items.filter((i) => i !== item);
+    setItems(updatedItems);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TextInput
+          style={styles.input}
+          value={inputValue}
+          onChangeText={setInputValue}
+          placeholder="Digite um item..."
+        />
+        <Button title="Adicionar" onPress={handleAddItem} />
+      </View>
+      <View style={styles.separator} />
+      <View>
+        {items.length === 0 ? (
+          <Text style={styles.title}>Lista vazia</Text>
+        ) : (
+          <FlatList
+            data={items}
+            renderItem={({ item }) => (
+              <View style={styles.listItem}>
+                <Text>{item}</Text>
+                <TouchableOpacity onPress={() => handleRemoveItem(item)}>
+                  <Text style={styles.removeButton}>Excluir</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  removeButton: {
+    color: 'red',
   },
 });
+
+export default App;
